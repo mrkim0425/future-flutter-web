e6276f4a68f1a37a3bd23d724b4b9c9c4438b763
김노트 <note11@kakao.com>
2024-08-02 01:30:00 +0900

    Update: Update with Additional Detail at coming_soon_text.dart

--- lib/home/coming_soon_text.dart
+++ lib/home/coming_soon_text.dart
@@ -3,40 +3,57 @@ import 'package:flutter/material.dart';
 class ComingSoonText extends StatelessWidget {
   const ComingSoonText({super.key});
 
+  static const baseSize = Size(1920, 1080);
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final width = size.width;
-    final height = size.height;
+    final size = MediaQuery.sizeOf(context);
+    final isLandscape = size.width > size.height;
 
-    final h1FontSize = width > height ? (width / 1920) * 240 : (height / 1080) * 100;
-    final h2FontSize = width > height ? (width / 1920) * 80 : (height / 1080) * 50;
-    final h3FontSize = width > height ? (width / 1920) * 52 : (height / 1080) * 26;
-    final h4FontSize = width > height ? (width / 1920) * 40 : (height / 1080) * 20;
+    final scaleByWidth = size.width / baseSize.width;
+    final scaleByHeight = size.height / baseSize.height;
+
+    final scaleByOrientation = isLandscape ? scaleByWidth : scaleByHeight;
+
+    final double h1FontSize = (scaleByWidth * (isLandscape ? 200 : 240))
+        .clamp(40.0, 160.0); // prevent text overflow
+    final double h2FontSize =
+        (scaleByOrientation * (isLandscape ? 80 : 50)).clamp(0.0, 64.0); // *1.6
+    final double h3FontSize =
+        (scaleByOrientation * (isLandscape ? 52 : 26)).clamp(0.0, 32); // *2
+    final double h4FontSize =
+        (scaleByOrientation * (isLandscape ? 40 : 20)).clamp(0.0, 24); // *2
 
     return Align(
       alignment: Alignment.center,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Future<Flutter>',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              height: 1.1,
-              fontSize: h1FontSize,
-              color: Colors.indigo.withOpacity(0.1),
-              fontWeight: FontWeight.w900,
-              shadows: [
-                Shadow(
-                  color: Colors.white.withOpacity(0.05),
-                  offset: const Offset(2, 2),
-                  blurRadius: 2,
-                ),
-              ],
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Expanded(
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Text(
+              'Future<Flutter>',
+              textAlign: TextAlign.center,
+              softWrap: false,
+              maxLines: 1,
+              overflow: TextOverflow.visible,
+              style: TextStyle(
+                height: 1.1,
+                fontSize: h1FontSize,
+                color: Colors.indigo.withOpacity(0.1),
+                fontWeight: FontWeight.w700,
+                shadows: [
+                  Shadow(
+                    color: Colors.white.withOpacity(0.05),
+                    offset: const Offset(2, 2),
+                    blurRadius: 2,
+                  ),
+                ],
+              ),
             ),
-          ),
-          Text(
+          ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40),
+          child: Text(
             'Coming Soon',
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -44,32 +61,35 @@ class ComingSoonText extends StatelessWidget {
               fontSize: h2FontSize,
               shadows: [
                 Shadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: const Offset(2, 2),
-                  blurRadius: 2,
+                  color: Colors.white.withOpacity(0.42),
+                  offset: const Offset(0, 0),
+                  blurRadius: 22,
                 ),
               ],
             ),
           ),
-          const SizedBox(height: 40),
-          Text(
-            '2024. 9. 28. Sat',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: h4FontSize,
+        ),
+        Expanded(
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Text(
+              '2024. 9. 28. Sat',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.32),
+                fontSize: h3FontSize,
+              ),
             ),
-          ),
-          Text(
-            'Incheon, Songdo Convensia',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: h3FontSize,
+            Text(
+              'Songdo Convensia, Incheon, Korea',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.32),
+                fontSize: h4FontSize,
+              ),
             ),
-          ),
-        ],
-      ),
+          ]),
+        ),
+      ]),
     );
   }
 }
